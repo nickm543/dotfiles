81d5d98285ab458e53d37b6f7c9d126d9610058e
Nick Medema <ntmedema@gmail.com>
2021-08-26 14:36:33 -0500

    Changed timezone

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -20,7 +20,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "America/Chicago";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
